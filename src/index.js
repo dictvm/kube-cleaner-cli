c65cc5fca41b8949cbda3f95f5d20d967884c3c7
Alexander Plavinski <alexander.plavinski@invision.de>
2019-03-14 10:44:55 +0100

    Add reading path

--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
 "use strict";
 const React = require("react");
 const { render, Color } = require("ink");
+const yml = require("js-yaml");
+const fs = require("fs");
+const homedir = require("os").homedir();
+const path = require("path");
 
-function useInterval(callback, delay) {
-  const savedCallback = React.useRef();
+// function useInterval(callback, delay) {
+//   const savedCallback = React.useRef();
 
+//   React.useEffect(() => {
+//     savedCallback.current = callback;
+//   });
+
+//   React.useEffect(() => {
+//     function tick() {
+//       savedCallback.current();
+//     }
+
+//     let id = setInterval(tick, delay);
+//     return () => clearInterval(id);
+//   }, [delay]);
+// }
+
+const KubeCleaner = () => {
+  const [cluster, setCluster] = React.useState(null);
   React.useEffect(() => {
-    savedCallback.current = callback;
-  });
+    const readConfig = async () => {
+      const file = path.join(homedir, ".kube/config");
+      console.log(file);
+    };
 
-  React.useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
+    readConfig();
+  }, []);
 
-    let id = setInterval(tick, delay);
-    return () => clearInterval(id);
-  }, [delay]);
-}
-
-const Counter = () => {
-  const [counter, setCounter] = React.useState(0);
-  useInterval(() => {
-    setCounter(counter + 1);
-  }, 100);
-
-  return <Color green>{counter} tests passed</Color>;
+  return <Color green>1 tests passed</Color>;
 };
 
-render(<Counter />);
+render(<KubeCleaner />);
